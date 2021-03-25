d5b49fbff857883a413f62ccfbe003906dbfd1eb
Prateek Sharma <prateekshr2@gmail.com>
2021-03-25 19:50:38 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+// Require dependencies
+const express = require('express');
+const dotenv = require('dotenv');
+
+// Load environment variables
+dotenv.config();
+
+// Setup server
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+  )
+);
